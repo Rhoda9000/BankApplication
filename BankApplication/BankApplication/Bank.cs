3c04c8cf3bfc4096eca905c9180df685643e7163
Rhoda9000 <dylanrhoda@aim.com>
2017-07-04 15:41:22 +0200

    Add files via upload

---
+++ BankApplication/BankApplication/Bank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    class Bank
+    {
+        private string AccountHolder;
+        private int AccountNumber;
+        private double Balance;
+
+        public Bank(string AccHold , int AccNum , double Bal)
+        {
+            AccountHolder = AccHold;
+            AccountNumber = AccNum;
+            Balance = Bal;
+        }
+
+        public void Deposit(double x)
+        {
+            Balance = Balance + x;
+        }
+
+        public void Withdraw(double y)
+        {
+            if(y > Balance)
+            {
+                Console.WriteLine("Insufficient Funds");
+                Console.WriteLine("\n");
+            }
+            else
+            {
+                Balance = Balance - y;
+                Console.WriteLine("\n");
+                Console.WriteLine("You Withdrew R" + y);
+                Console.WriteLine("\n");
+            }
+        }
+
+        public double getBalance()
+        {
+            return Balance;
+        }
+
+    }
+}
---
+++ BankApplication/BankApplication/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string AccHold;
+            int AccNum;
+
+            Console.WriteLine("What is The Account Holder's Name? ");
+            AccHold = Console.ReadLine();
+
+            Console.WriteLine("Enter Bank Account Number");
+            AccNum = int.Parse(Console.ReadLine());
+
+            Bank Account = new Bank(AccHold, AccNum, 0.00);
+
+            Console.WriteLine("\n");
+
+            bool exit = true;
+            while (exit)
+            {
+                Console.WriteLine("Welcome To Basic Bank Application");
+                Console.WriteLine("1.Check Balance");
+                Console.WriteLine("2.Deposit");
+                Console.WriteLine("3.Withdraw");
+                Console.WriteLine("4.Display Account Details");
+                Console.WriteLine("5.Exit");
+                Console.WriteLine("Select an option");
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("\n");
+                        Console.WriteLine("Your Balance is R" + Account.getBalance());
+                        Console.WriteLine("\n");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("How Much Do You Want To Deposit?");
+                        double x = int.Parse(Console.ReadLine());
+                        Account.Deposit(x);
+                        Console.WriteLine("\n");
+                        Console.WriteLine("You Have Deposited R" + x);
+                        Console.WriteLine("\n");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("How Much Do You Want To Withdraw?");
+                        double y = int.Parse(Console.ReadLine());
+                        Account.Withdraw(y);                       
+                        Console.WriteLine("\n");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("\n");
+                        Console.WriteLine("Account Holder's Name: " + AccHold + "\n" + "Account Number:" + AccNum + "\n" + "Balance: R" + Account.getBalance());
+                        Console.WriteLine("\n");
+                        break;
+
+
+                    case 5:
+                        exit = false;
+                        Console.WriteLine("\n");
+                        Console.WriteLine("Goodbye " + AccHold);
+                        break;
+
+                    default:
+                        Console.WriteLine("\n");
+                        Console.WriteLine("Invalid option");
+                        Console.WriteLine("\n");
+                        break;
+                }
+            }
+        }
+    }
+}
